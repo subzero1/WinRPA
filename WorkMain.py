709fca06f46a0d232089362e45c11cb2d001bc0e
subzero1 <liaojialv@gmail.com>
2023-06-01 08:35:19 +0800

    Update:Add task kill feature command

--- WorkMain.py
+++ WorkMain.py
@@ -5,14 +5,15 @@ import win32api
 import win32con
 import win32gui
 
+
 #
 # 输出当前活动窗体句柄
 #
 def print_GetForegroundWindow():
     hwnd_active = win32gui.GetForegroundWindow()
-    print('hwnd_active hwnd:',hwnd_active)
-    print('hwnd_active text:',win32gui.GetWindowText(hwnd_active))
-    print('hwnd_active class:',win32gui.GetClassName(hwnd_active))
+    print('hwnd_active hwnd:', hwnd_active)
+    print('hwnd_active text:', win32gui.GetWindowText(hwnd_active))
+    print('hwnd_active class:', win32gui.GetClassName(hwnd_active))
 
 try:
     os.system("netsh interface set interface \"WLAN2\" admin=disabled")
@@ -27,6 +28,6 @@ try:
     pyautogui.click(960, 625)
     time.sleep(16)
     os.system("netsh interface set interface \"WLAN2\" admin=enabled")
+    os.system("taskkill /F /IM CloudClient.exe")
 except Exception as e:
     print("窗口句柄获取失败或是前台设置失败：{}".format(e))
-
